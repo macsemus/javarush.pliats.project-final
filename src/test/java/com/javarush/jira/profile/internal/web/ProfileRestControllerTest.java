62c89de0089e83d7c52ace59ff26bf4d3eb85896
Максим <makcarno@gmail.com>
2024-11-04 04:54:19 +0300

    test for ProfileRestController

--- src/test/java/com/javarush/jira/profile/internal/web/ProfileRestControllerTest.java
+++ src/test/java/com/javarush/jira/profile/internal/web/ProfileRestControllerTest.java
@@ -1,8 +1,75 @@
 package com.javarush.jira.profile.internal.web;
 
 import com.javarush.jira.AbstractControllerTest;
+import com.javarush.jira.profile.internal.ProfileRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithUserDetails;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static com.javarush.jira.common.util.JsonUtil.writeValue;
+import static com.javarush.jira.login.internal.web.UserTestData.*;
+import static com.javarush.jira.profile.internal.web.ProfileTestData.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 
 class ProfileRestControllerTest extends AbstractControllerTest {
+    private static final String REST_URL = ProfileRestController.REST_URL;
 
+    @Autowired
+    private ProfileRepository profileRepository;
+
+    @Test
+    public void unauthorizedUserGet() throws Exception {
+        perform(MockMvcRequestBuilders.get(ProfileRestController.REST_URL))
+                .andExpect(status().isUnauthorized());
+    }
+
+
+    @Test
+    @WithUserDetails(value = USER_MAIL)
+    public void authorizedUserGet() throws Exception {
+        perform(MockMvcRequestBuilders.get(ProfileRestController.REST_URL))
+                .andExpect(status().isOk())
+                .andDo(print())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+                .andExpect(PROFILE_TO_MATCHER.contentJson(USER_PROFILE_TO));
+    }
+
+    @Test
+    @WithUserDetails(value = GUEST_MAIL)
+    public void guestUserGet() throws Exception {
+        perform(MockMvcRequestBuilders.get(ProfileRestController.REST_URL))
+                .andExpect(status().isOk())
+                .andDo(print())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+                .andExpect(PROFILE_TO_MATCHER.contentJson(GUEST_PROFILE_EMPTY_TO));
+    }
+
+    @Test
+    void unauthorizedUserUpdate() throws Exception {
+        perform(MockMvcRequestBuilders.put(REST_URL)
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isUnauthorized());
+    }
+
+    @Test
+    @WithUserDetails(value = ADMIN_MAIL)
+    void authorizedUserUpdate() throws Exception {
+        perform(MockMvcRequestBuilders.put(REST_URL)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(writeValue(USER_PROFILE_TO)))
+                .andExpect(status().is2xxSuccessful());
+    }
+
+    @Test
+    @WithUserDetails(value = ADMIN_MAIL)
+    public void isNoContentUpdate() throws Exception {
+        perform(MockMvcRequestBuilders.put(REST_URL)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(writeValue(USER_PROFILE_TO)))
+                .andExpect(status().isNoContent());
+    }
 }
